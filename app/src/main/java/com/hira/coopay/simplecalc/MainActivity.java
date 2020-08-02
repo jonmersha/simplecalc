0b12e63d347f55e0c2a4c1f3f8d25a0e659294d0
jonmersha <jonmersa@gmail.com>
2020-08-02 13:54:47 +0300

    Add Functionalities Included

--- app/src/main/java/com/hira/coopay/simplecalc/MainActivity.java
+++ app/src/main/java/com/hira/coopay/simplecalc/MainActivity.java
@@ -11,12 +11,13 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     EditText input;
-
     Button add;
     Button subtract;
     Button division;
     Button multiplication;
     Button equalTo;
+
+    int op1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         input = findViewById(R.id.input);
         add = findViewById(R.id.add);
+
         subtract = findViewById(R.id.subb);
         division = findViewById(R.id.div);
         multiplication = findViewById(R.id.mult);
@@ -34,5 +36,13 @@ public class MainActivity extends AppCompatActivity {
         int id=v.getId();
         Button button=findViewById(id);
         input.append(button.getText().toString());
+    }
+    public void add(View v){
+     op1= Integer.parseInt(input.getText().toString());
+     input.setText("");
+    }
+    public void equalTo(View v){
+        int result= op1+Integer.parseInt(input.getText().toString());
+        input.setText(""+result);
     }
 }
